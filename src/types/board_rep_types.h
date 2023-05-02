59a9f50011d8b909de07935b96df3b56948d0691
Archishmaan <archishou@gmail.com>
2023-05-02 19:21:13 -0400

    update

--- src/types/board_rep_types.h
+++ src/types/board_rep_types.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "../types.h"
 
+using ZobristHash = u64;
+using Bitboard = u64;
+
 constexpr i8 NCOLORS = 2;
 enum Color : u8 {
 	WHITE,
@@ -10,6 +13,43 @@ enum Color : u8 {
 constexpr Color operator~(Color c) {
 	return Color(c ^ BLACK);
 }
+
+constexpr i8 NDIRS = 8;
+enum Direction : i32 {
+	NORTH = 8, NORTH_EAST = 9, EAST = 1, SOUTH_EAST = -7,
+	SOUTH = -8, SOUTH_WEST = -9, WEST = -1, NORTH_WEST = 7,
+	NORTH_NORTH = 16, SOUTH_SOUTH = -16
+};
+
+template<Color C, Direction D>
+constexpr Direction relative_dir() {
+	if constexpr (C == WHITE) return D;
+	return Direction(-D);
+}
+
+constexpr u32 NFILES = 8;
+using File = u32;
+
+constexpr File AFILE = 0;
+constexpr File BFILE = 1;
+constexpr File CFILE = 2;
+constexpr File DFILE = 3;
+constexpr File EFILE = 4;
+constexpr File FFILE = 5;
+constexpr File GFILE = 6;
+constexpr File HFILE = 7;
+
+constexpr u32 NRANKS = 8;
+using Rank = u32;
+
+constexpr Rank RANK1 = 0;
+constexpr Rank RANK2 = 1;
+constexpr Rank RANK3 = 2;
+constexpr Rank RANK4 = 3;
+constexpr Rank RANK5 = 4;
+constexpr Rank RANK6 = 5;
+constexpr Rank RANK7 = 6;
+constexpr Rank RANK8 = 7;
 
 constexpr u32 NSQUARES = 64;
 enum Square : u32 {
@@ -24,44 +64,6 @@ enum Square : u32 {
 	NO_SQUARE
 };
 
-
-const i8 NDIRS = 8;
-enum Direction : i32 {
-	NORTH = 8, NORTH_EAST = 9, EAST = 1, SOUTH_EAST = -7,
-	SOUTH = -8, SOUTH_WEST = -9, WEST = -1, NORTH_WEST = 7,
-	NORTH_NORTH = 16, SOUTH_SOUTH = -16
-};
-
-template<Color C, Direction D>
-constexpr Direction relative_dir() {
-	if constexpr (C == WHITE) return D;
-	return Direction(-D);
-}
-
-const u32 NFILES = 8;
-using File = u32;
-
-constexpr File AFILE = 0;
-constexpr File BFILE = 1;
-constexpr File CFILE = 2;
-constexpr File DFILE = 3;
-constexpr File EFILE = 4;
-constexpr File FFILE = 5;
-constexpr File GFILE = 6;
-constexpr File HFILE = 7;
-
-const u32 NRANKS = 8;
-using Rank = u32;
-
-constexpr Rank RANK1 = 0;
-constexpr Rank RANK2 = 1;
-constexpr Rank RANK3 = 2;
-constexpr Rank RANK4 = 3;
-constexpr Rank RANK5 = 4;
-constexpr Rank RANK6 = 5;
-constexpr Rank RANK7 = 6;
-constexpr Rank RANK8 = 7;
-
 const u32 NCASTLING_RIGHTS = 4;
 using CastleRight = u32;
 
@@ -69,6 +71,3 @@ constexpr CastleRight BLACK_OO	= 0b0001;
 constexpr CastleRight BLACK_OOO = 0b0010;
 constexpr CastleRight WHITE_OO	= 0b0100;
 constexpr CastleRight WHITE_OOO = 0b1000;
-
-using ZobristHash = u64;
-using Bitboard = u64;
